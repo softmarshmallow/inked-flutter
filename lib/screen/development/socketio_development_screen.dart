518780c84feeefae8a34768f458bfae785d915a3
woojoo <woojoo@softmarshmallow.com>
2020-03-26 09:24:27 +0900

    😣updated server endpoint. service back online!

--- lib/screen/development/socketio_development_screen.dart
+++ lib/screen/development/socketio_development_screen.dart
@@ -3,6 +3,7 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketioDevelopmentScreen extends StatefulWidget {
   static const routeName = "/development/socketio";
+
   SocketioDevelopmentScreen({Key key}) : super(key: key);
 
   @override
@@ -11,22 +12,38 @@ class SocketioDevelopmentScreen extends StatefulWidget {
 }
 
 class _SocketioDevelopmentScreenState extends State<SocketioDevelopmentScreen> {
-
+  String message = "";
 
   @override
   void initState() {
     super.initState();
-    IO.Socket socket = IO.io('http://13.209.232.176:3001', <String, dynamic>{
-      'transports': ['websocket'],
+    IO.Socket socket = IO.io('http://localhost:3001/client', <String, dynamic>{
+//      'transports': ['websocket'],
     });
     socket.on('connect', (_) {
       print('connect');
-      socket.emit('my other event', 'test');
+      socket.emitWithAck('mocknews', '', ack:(d){
+        print(d);
+        setState(() {
+          message = d;
+        });
+      });
+      socket.on('feed', (data) {
+        print(data);
+        setState(() {
+          message = data;
+        });
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('socket io'),),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('socket io'),
+      ),
+      body: Text(message),
+    );
   }
 }
