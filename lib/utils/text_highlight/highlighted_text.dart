00fdfa46d488e3170613b33d614c61ca7e74a603
woojoo <woojoo@softmarshmallow.com>
2020-04-03 03:28:43 +0900

    🤪‍ added es search & highlighting

--- lib/utils/text_highlight/highlighted_text.dart
+++ lib/utils/text_highlight/highlighted_text.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
 Widget buildTextFromEm(String emString, TextStyle style){
-  emString = emString.replaceAll("<em>", " _**");
-  emString = emString.replaceAll("</em>", "**_ ");
+  emString = emString.replaceAll("<em>", " ***");
+  emString = emString.replaceAll("</em>", "*** ");
   return Markdown(data: emString, shrinkWrap: true,);
 }
