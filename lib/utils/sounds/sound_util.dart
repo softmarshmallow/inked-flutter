ebc64d395e43540eb082aec36f0b5e1eeb057682
woojoo <woojoo@softmarshmallow.com>
2020-04-24 00:36:06 +0900

    🤩 fixed playshot windows platform issue

--- lib/utils/sounds/sound_util.dart
+++ lib/utils/sounds/sound_util.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:inked/data/remote/lite_connector.dart';
 
@@ -6,9 +8,13 @@ AudioPlayer audioPlayer = AudioPlayer();
 List<String> _played = [];
 playOnceInLifetime(String id, String file) async {
   if (!_played.contains(id)) {
-    var res = await audioPlayer.play(file);
-    print("played sound $file -> $res");
+    if (Platform.isWindows || Platform.isLinux) {
+      LiteAppConnector().alertOnLiteApp(id);
+      print("played sound $file -> via lite app");
+    }  else{
+      var res = await audioPlayer.play(file);
+      print("played sound $file -> $res");
+    }
     _played.add(id);
-    LiteAppConnector().alertOnLiteApp(id);
   }
 }
